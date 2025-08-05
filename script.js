2d27fdb3d1771706c9f91f243850475d800b5b44
anind <anindita.salian@gmail.com>
2025-08-05 21:53:42 +0530

    can change grid size, added color picker

--- script.js
+++ script.js
@@ -1,18 +1,90 @@
 const canvas = document.querySelector(".canvas");
 
-for (let i = 1; i<=16; i++) {
-    for (let j = 1; j<=16; j++) {
-        const addGrid = document.createElement("div");
-        addGrid.classList.add("grid");
-        //grid.textContent = `${j}`
-        canvas.appendChild(addGrid);
+let gridSize = 16;
+let paintColor = "black";
+
+const gridInput = document.querySelector("#gridInput");
+const create = document.querySelector(".create");
+
+
+create.addEventListener("click", (e) => {
+    const gridInputVal = Number(gridInput.value);
+    if (!isNaN(gridInputVal) && gridInputVal > 0 && gridInputVal <=100) {
+        gridSize = gridInputVal;
+        clearGrid();
+        createGrid();
+    } 
+})
+
+
+function createGrid() {
+    for (let i = 1; i <= gridSize; i++) {
+        for (let j = 1; j <= gridSize; j++) {
+            const addGrid = document.createElement("div");
+            let size = (512 / gridSize);
+            addGrid.style.height = `${size}px`;
+            addGrid.style.width = `${size}px`;
+            addGrid.classList.add("grid");
+            canvas.appendChild(addGrid);
+
+            addGrid.addEventListener("mouseenter", () => {
+                addGrid.style.backgroundColor = paintColor;
+            })
+        }
     }
 }
 
+createGrid();
+
 const grids = document.querySelectorAll(".grid");
 
-grids.forEach(grid => {
-    grid.addEventListener("mouseenter", (e) => {
-        grid.style.backgroundColor = "black";
-    })
+function clearGrid() {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
+}
+
+const black = document.querySelector(".black");
+black.addEventListener("click", (e) => {
+    paintColor = "black";
+})
+
+const red = document.querySelector(".red");
+red.addEventListener("click", (e) => {
+    paintColor = "red";
+})
+
+const blue = document.querySelector(".blue");
+blue.addEventListener("click", (e) => {
+    paintColor = "blue";
+})
+
+const yellow = document.querySelector(".yellow");
+yellow.addEventListener("click", (e) => {
+    paintColor = "yellow";
+})
+
+const green = document.querySelector(".green");
+green.addEventListener("click", (e) => {
+    paintColor = "green";
+})
+
+const purple = document.querySelector(".purple");
+purple.addEventListener("click", (e) => {
+    paintColor = "purple";
+})
+
+const orange = document.querySelector(".orange");
+orange.addEventListener("click", (e) => {
+    paintColor = "orange";
+})
+
+const pink = document.querySelector(".pink");
+pink.addEventListener("click", (e) => {
+    paintColor = "pink";
+})
+
+const gray = document.querySelector(".gray");
+gray.addEventListener("click", (e) => {
+    paintColor = "gray";
 })
